3d4a07847235b6055e818716cbd6cd9c285f96b8
I am Alaska <stephen.alaska94@hotmail.com>
2023-02-08 09:08:41 +0000

    complete about page design for desktop and mobile version

---
+++ js/about.js
@@ -0,0 +1,123 @@
+const aboutPage = () => `      <section id="about-headline" class="about-headline">
+    <div class="container">
+      <h5 class="headline-subtitle d-block d-sm-block d-md-none d-lg-none">
+        "Be a Part of Musical History!"
+      </h5>
+      <h1 class="headline-title fw-bolder">
+        THE BIGGEST MUSIC EVENT OF THE YEAR
+      </h1>
+
+      <div class="event-summary card border border-4">
+        <div class="card-body p-3 fw-semibold">
+          <p class="mb-0">
+            Rock Fest features many different performersm typically spread
+            over two or three days and having a campsite and other amenities
+            and forms of entertainment provided at the venue.
+          </p>
+
+          <p class="d-none d-sm-none d-md-block d-lg-block d-xl-block">
+            So if you're active and engaged in the worlds of open content
+            and knowledge - free software advocates, Wikipedians, Open
+            Knowledge, galleries, libraries, museums, archives, governments
+            and foundations, lawyers and activists - we hope you'll consider
+            joining us this year to build a stronger, more vibrant commons
+            together.
+          </p>
+        </div>
+      </div>
+
+      <div class="contact-summary my-4">
+        <p class="mb-1">
+          Please contact us per email for any further question about rock
+          fest!
+        </p>
+        <a href="mailto:rockfest@yahoo.com">rockfest@yahoo.com</a>
+      </div>
+    </div>
+  </section>
+
+  <section id="about-logo-copyright">
+    <div class="container py-5 px-2">
+      <h1
+        class="copyright-section-title section-title fw-bolder text-center"
+      >
+        Creative Commons Global Summit 2015 Logo
+      </h1>
+
+      <div class="logo-copyright-text">
+        <p class="mb-0">
+          The logo of CC Global Summit 2015 was decided through the logo
+          competition from 8 June to 7 July.
+        </p>
+
+        <p class="d-none d-sm-none d-md-block d-lg-block d-xl-block">
+          After online public voting and selection committee's juding
+          process, the logo from Naresh Afrawal won the prize. After
+          consultation with Mr. Agrawal we optimized the prize-winning-log
+          for this year's summit as following.
+        </p>
+      </div>
+
+      <div class="logo-display card mt-5">
+        <div class="card-body">
+          <div
+            class="logo d-flex align-items-center justify-content-center gap-2"
+          >
+            <img
+              src="assets/images/rock_fest_logo.jpg"
+              width="90px"
+              alt="rock fest"
+            />
+            <h5 class="logo-name">
+              rock<span class="logo-name-orange">fest</span>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="event-history" class="border border-top border-1">
+    <div class="container py-5 px-2">
+      <h1 class="history-section-title section-title fw-bolder text-center">
+        See the past Rock Fest Events
+      </h1>
+
+      <p class="history-text">
+        Take a look at the last two Rock Fest Event which took place in
+        Buenos Aires and in Warsaw
+      </p>
+
+      <div class="event-list mt-5">
+        <div class="row gap-3 gap-md-0">
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="event-history-card card">
+              <div class="card-body">
+                <div
+                  class="event-record text-center position-absolute text-white"
+                >
+                  <h1 class="fw-bolder">2013</h1>
+                  <p>Rock Summer Festival 2013 in Buenos Aires</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+            <div class="event-history-card card">
+              <div class="card-body">
+                <div
+                  class="event-record text-center position-absolute text-white"
+                >
+                  <h1 class="fw-bolder">2011</h1>
+                  <p>Rock Summer Festival 2011 in Warsaw</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>`;
+
+export default aboutPage;
